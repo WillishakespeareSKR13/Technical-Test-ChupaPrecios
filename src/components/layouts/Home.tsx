f92cb7e41e8b4696d8a755f008b095cfeb504329
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 16:18:22 -0700

    :sparkles: feat: update Home layout component

--- src/components/layouts/Home.tsx
+++ src/components/layouts/Home.tsx
@@ -1,25 +1,30 @@
-import AtomWrapper from "@/components/atoms/AtomWrapper";
 import FooterPublic from "@/components/complex/Footers/Public";
 import HeaderPublic from "@/components/complex/Headers/Public";
 import { css } from "@kuma-ui/core";
 
+import AtomElement from "../atoms/AtomElement";
+
+import { AtomElementProps } from "../atoms/AtomElement/types";
+
 type Props = {
   children: React.ReactNode;
+  wrapper?: AtomElementProps;
 };
 
 const LayoutHome = (props: Props) => {
-  const { children } = props;
+  const { children, wrapper } = props;
   return (
     <>
       <HeaderPublic />
-      <AtomWrapper
+      <AtomElement
         as="main"
         css={css`
           min-height: 100vh;
         `}
+        {...wrapper}
       >
         {children}
-      </AtomWrapper>
+      </AtomElement>
       <FooterPublic />
     </>
   );
