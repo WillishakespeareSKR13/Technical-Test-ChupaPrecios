9851dddd7fd76e35f906dc0e405847b930847356
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-14 02:23:18 -0700

    :sparkles: feat: add transition to color in AtomText CSS and import AtomText in OpenCart.tsx

--- src/components/complex/Butons/OpenCart.tsx
+++ src/components/complex/Butons/OpenCart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AtomButton from "@/components/atoms/AtomButton";
+import AtomText from "@/components/atoms/AtomText";
 import { CartShopAtom } from "@/jotai/CarShop";
 import { css } from "@kuma-ui/core";
 import { useAtomValue } from "jotai";
@@ -12,15 +13,49 @@ type Props = {
 const ButtonOpenCart = (props: Props) => {
   const { children } = props;
   const carShops = useAtomValue(CartShopAtom);
-  console.log(carShops);
+
+  const hasProducts = carShops.length > 0;
+
   return (
     <AtomButton
       onClick={() => {}}
       astype="outline"
       css={css`
-        padding: 0 12px;
+        position: relative;
+        padding: 0 12px !important;
+        &:hover {
+          span {
+            background-color: var(--color-primary);
+            border: 1px solid var(--background-light);
+          }
+        }
       `}
     >
+      {hasProducts && (
+        <AtomText
+          css={css`
+            position: absolute;
+            top: 0px;
+            right: 0px;
+            background-color: var(--background-light);
+            border: var(--border);
+            box-shadow: var(--box-shadow);
+            color: var(--color-primary);
+            font-size: var(--font-size-small);
+            font-weight: var(--font-weight-bold);
+            border-radius: var(--border-radius);
+            min-width: 24px;
+            padding: 0 6px;
+            height: 24px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transform: translate(60%, -60%);
+          `}
+        >
+          {Math.min(99, carShops.length)} {carShops.length > 99 && "+"}
+        </AtomText>
+      )}
       {children}
     </AtomButton>
   );
