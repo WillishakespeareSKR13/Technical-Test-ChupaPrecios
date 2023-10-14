2627927db731328317caede22975f3f7f335697b
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-14 03:36:53 -0700

    :art: refactor: update styling for PriceProduct component

--- src/components/complex/Tags/PriceProduct.tsx
+++ src/components/complex/Tags/PriceProduct.tsx
@@ -1,6 +1,7 @@
 import AtomText from "@/components/atoms/AtomText";
 import AtomWrapper from "@/components/atoms/AtomWrapper";
 import { IProduct } from "@/types/product";
+import { cv } from "@/utils/css";
 import { css } from "@kuma-ui/core";
 
 type Props = {
@@ -15,32 +16,80 @@ const TagPriceProduct = (props: Props) => {
   return (
     <AtomWrapper
       css={css`
+        flex-direction: column;
+        align-items: flex-end;
         position: absolute;
         bottom: 8px;
         right: 8px;
         z-index: 1;
-        background-color: var(--color-primary);
-        border: 1px solid var(--color-primary-light);
-        box-shadow: var(--box-shadow);
-        color: var(--color-primary);
-        font-size: var(--font-size-small);
-        font-weight: var(--font-weight-bold);
-        border-radius: var(--border-radius);
-        padding: var(--space-small) var(--space-regular);
-        height: 32px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        span {
-          font-size: 12px;
-          font-weight: 700;
-          color: var(--color-text-light);
-        }
+        gap: var(--space-small);
       `}
     >
-      <AtomText>$ {product?.price}</AtomText>
+      <AtomWrapper css={cvOffer({ offer: product?.hasOffer })}>
+        <AtomText>$ {product?.price?.toFixed(2)}</AtomText>
+      </AtomWrapper>
+      {product?.hasOffer && (
+        <AtomWrapper
+          css={css`
+            background-color: var(--color-primary);
+            border: 1px solid var(--color-primary-light);
+            box-shadow: var(--box-shadow);
+            color: var(--color-primary);
+            font-size: var(--font-size-small);
+            font-weight: var(--font-weight-bold);
+            border-radius: var(--border-radius);
+            padding: var(--space-small) var(--space-regular);
+            height: 32px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            span {
+              font-size: 12px;
+              font-weight: 700;
+              color: var(--color-text-light);
+            }
+          `}
+        >
+          <AtomText>Offer $ {product?.priceOffer?.toFixed(2)}</AtomText>
+        </AtomWrapper>
+      )}
     </AtomWrapper>
   );
 };
 
 export default TagPriceProduct;
+
+const cssTag = css`
+  width: max-content;
+  background-color: var(--background-light);
+  border: var(--border);
+  box-shadow: var(--box-shadow);
+  font-size: var(--font-size-small);
+  font-weight: var(--font-weight-bold);
+  border-radius: var(--border-radius);
+  padding: var(--space-small) var(--space-regular);
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 12px;
+    font-weight: 700;
+    color: var(--color-text);
+  }
+`;
+
+const cssOffer = css`
+  background-color: var(--background) !important;
+  border: var(--border) !important;
+  box-shadow: var(--box-shadow) !important;
+  span {
+    color: var(--color-text-alt) !important;
+    text-decoration: line-through;
+  }
+`;
+
+const cvOffer = cv({
+  base: cssTag,
+  offer: cssOffer,
+});
