57b6fbe1a4e9c655cf9d0f8a15b8d0c8e8a5d751
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 20:44:11 -0700

    feat: add astheme prop to AtomText component

--- src/components/atoms/AtomText/types.ts
+++ src/components/atoms/AtomText/types.ts
@@ -1,7 +1,9 @@
+import { ThemeKey } from "@/types/theme";
 import type { HTMLAttributes, DetailedHTMLProps } from "react";
 
 type Text = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>;
 
 export type AtomTextProps = Omit<Text, "ref"> & {
   css?: string;
+  astheme?: ThemeKey;
 };
