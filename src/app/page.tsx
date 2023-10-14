fc627860b2c10aeff921fc8992840f5ef61b9ecb
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 23:17:51 -0700

    feat: add AtomElement and CardProduct components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
-import AtomButton from "@/components/atoms/AtomButton";
+import AtomElement from "@/components/atoms/AtomElement";
+import CardProduct from "@/components/complex/Cards/Product";
 import LayoutHome from "@/components/layouts/Home";
+import { IProduct, PRODUCTS } from "@/constants/product";
+import { css } from "@kuma-ui/core";
 
-const Home = () => (
-  <LayoutHome>
-    <AtomButton>This is a button</AtomButton>
-  </LayoutHome>
-);
+const Products = async () => {
+  const PromiseProducts = new Promise<IProduct[]>((resolve) => {
+    // setTimeout(() => {
+    //   resolve(PRODUCTS);
+    // }, 2000);
+    resolve(PRODUCTS);
+  });
+  const response = await PromiseProducts;
+  return response;
+};
+
+const Home = async () => {
+  const products = await Products();
+  return (
+    <LayoutHome>
+      <AtomElement
+        as="section"
+        css={css`
+          display: grid !important;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+          padding: var(--space-padding);
+          grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+          gap: var(--space-regular);
+        `}
+      >
+        {products?.map((item) => (
+          <CardProduct key={item.id} {...item} />
+        ))}
+      </AtomElement>
+    </LayoutHome>
+  );
+};
 
 export default Home;
