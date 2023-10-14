64ca15a0ef62d7af41b8313d03d3f67907054852
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-14 03:15:01 -0700

    feat: add priceOffer and hasOffer properties to product type

--- src/types/product.ts
+++ src/types/product.ts
@@ -2,6 +2,8 @@ export type IProduct = {
   id: number;
   title: string;
   price: number;
+  priceOffer: number;
+  hasOffer: boolean;
   description: string;
   category: string;
   image: string;
