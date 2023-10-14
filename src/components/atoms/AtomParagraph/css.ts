c418f55e8c64228343725a4aefe1426a1291c99d
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 17:02:53 -0700

    :art: refactor: update AtomParagraph styling

--- src/components/atoms/AtomParagraph/css.ts
+++ src/components/atoms/AtomParagraph/css.ts
@@ -3,14 +3,10 @@ import { css } from "@kuma-ui/core";
 import { AtomParagraphProps } from "./types";
 
 export const cssParagraph = css`
-  width: max-content;
-  height: max-content;
-  padding: 8px 22px;
-  border-radius: var(--border-radius);
-  font-size: 12px;
-  line-height: 16px;
-  text-align: center;
-  cursor: pointer;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.5;
+  color: var(--text-color-alt);
 `;
 
 export const stylesParagraph = (props: AtomParagraphProps) => {
