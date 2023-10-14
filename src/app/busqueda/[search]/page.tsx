09c19338877b9dc651a46091678a2ff6cfa47a4d
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 18:07:50 -0700

    ✨feat: add AtomButton component toSearch

---
+++ src/app/busqueda/[search]/page.tsx
@@ -0,0 +1,10 @@
+import AtomButton from "@/components/atoms/AtomButton";
+import LayoutHome from "@/components/layouts/Home";
+
+const SearchBySearch = () => (
+  <LayoutHome>
+    <AtomButton>This is a button</AtomButton>
+  </LayoutHome>
+);
+
+export default SearchBySearch;
