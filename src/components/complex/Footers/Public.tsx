1992fae9bb8630a70fd35f13ddbfd0cefef41935
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 18:07:46 -0700

    :sparkles: feat: add sticky footer to Public component

--- src/components/complex/Footers/Public.tsx
+++ src/components/complex/Footers/Public.tsx
@@ -1,5 +1,23 @@
+import AtomElement from "@/components/atoms/AtomElement";
+import { css } from "@kuma-ui/core";
+
 const FooterPublic = () => {
-  return <div>FooterPublic</div>;
+  return (
+    <AtomElement
+      as="footer"
+      css={css`
+        position: sticky;
+        bottom: 0;
+        flex-direction: column;
+        background-color: var(--background-dark);
+      `}
+    >
+      asdsd
+      <AtomElement as="small">
+        Copyright © Chupaprecios all rights reserved. Powered by Chupaprecio
+      </AtomElement>
+    </AtomElement>
+  );
 };
 
 export default FooterPublic;
