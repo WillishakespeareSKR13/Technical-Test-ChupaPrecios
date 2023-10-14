6c2dc607994c141659675293af81a3f2bf3234d3
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 20:12:53 -0700

    ✨feat: update not-found component with new text and link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,33 @@
 import AtomHeading from "@/components/atoms/AtomHeading";
-import AtomText from "@/components/atoms/AtomText";
+import AtomParagraph from "@/components/atoms/AtomParagraph";
 import LayoutHome from "@/components/layouts/Home";
 import { css } from "@kuma-ui/core";
+import Link from "next/link";
 
 const NotFound = () => (
   <LayoutHome
     wrapper={{
       css: css`
+        display: flex;
+        flex: 1;
         justify-content: center;
         align-items: center;
       `,
     }}
   >
     <AtomHeading>Pagina no encontrada</AtomHeading>
-    <AtomText>No se ha encontrado la pagina que buscas</AtomText>
+    <AtomParagraph>Lo sentimos, creemos que te has perdido.</AtomParagraph>
+    <Link
+      href="/"
+      className={css`
+        font-size: 14px;
+        font-weight: 500;
+        margin-top: var(--space-regular);
+        color: var(--color-primary);
+      `}
+    >
+      Regresar al inicio
+    </Link>
   </LayoutHome>
 );
 
