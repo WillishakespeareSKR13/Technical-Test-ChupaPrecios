df814a1194290407e2bd537cf78d344cf82efa2f
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-14 04:25:27 -0700

    feat: add ToggleTheme component

---
+++ src/components/complex/Toggles/Theme.tsx
@@ -0,0 +1,24 @@
+"use client";
+import theme from "@/theme";
+import AtomButton from "../../atoms/AtomButton";
+import AtomIcon from "@/components/atoms/AtomIcon";
+
+const ToggleTheme = () => {
+  const [key] = theme.use();
+  return (
+    <AtomButton
+      astype="solid"
+      onClick={() => {
+        theme.toggle();
+      }}
+    >
+      {key === "light" ? (
+        <AtomIcon astheme="light" icon="moon" />
+      ) : (
+        <AtomIcon astheme="light" icon="sun" />
+      )}
+    </AtomButton>
+  );
+};
+
+export default ToggleTheme;
