0c2c8da497ffbd9ff210244be6dad78055b54ff9
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-14 01:05:34 -0700

    ✨feat: add ButtonAdd component

---
+++ src/components/complex/Butons/AddCart.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import AtomButton from "@/components/atoms/AtomButton";
+import { CartShopAtom } from "@/jotai/CarShop";
+import { IProduct } from "@/types/product";
+import { css } from "@kuma-ui/core";
+import { useSetAtom } from "jotai";
+
+type Props = {
+  product: IProduct;
+  children: React.ReactNode;
+};
+
+const ButtonAddCart = (props: Props) => {
+  const { product, children } = props;
+  const CarShop = useSetAtom(CartShopAtom);
+  return (
+    <AtomButton
+      onClick={() =>
+        CarShop({
+          type: "ADD",
+          payload: product,
+        })
+      }
+      astype="outline"
+      css={css`
+        padding: 0 12px;
+      `}
+    >
+      {children}
+    </AtomButton>
+  );
+};
+
+export default ButtonAddCart;
