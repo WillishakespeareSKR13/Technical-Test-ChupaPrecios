abd11f8ab7538b1dc24ed078e372c6a3f5e4cba1
William Jesus Covarrubias Ramos <willishakespeare@live.com.mx>
2023-10-13 17:02:49 -0700

    :art: refactor: update AtomHeading styles

--- src/components/atoms/AtomHeading/css.ts
+++ src/components/atoms/AtomHeading/css.ts
@@ -1,20 +1,51 @@
-import { cx } from "@/utils/css";
+import { cx, cv } from "@/utils/css";
 import { css } from "@kuma-ui/core";
 import { AtomHeadingProps } from "./types";
 
 export const cssHeading = css`
-  width: max-content;
-  height: max-content;
-  padding: 8px 22px;
-  border-radius: var(--border-radius);
-  font-size: 12px;
-  line-height: 16px;
-  text-align: center;
-  cursor: pointer;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.5;
+  color: var(--text-color);
 `;
 
+const cssHeadingH6 = css`
+  font-size: 14px;
+`;
+
+const cssHeadingH5 = css`
+  font-size: 16px;
+`;
+
+const cssHeadingH4 = css`
+  font-size: 18px;
+`;
+
+const cssHeadingH3 = css`
+  font-size: 20px;
+`;
+
+const cssHeadingH2 = css`
+  font-size: 24px;
+`;
+
+const cssHeadingH1 = css`
+  font-size: 32px;
+`;
+
+export const cvAsType = cv({
+  base: cssHeading,
+  h1: cssHeadingH1,
+  h2: cssHeadingH2,
+  h3: cssHeadingH3,
+  h4: cssHeadingH4,
+  h5: cssHeadingH5,
+  h6: cssHeadingH6,
+});
+
 export const stylesHeading = (props: AtomHeadingProps) => {
-  const { className, css } = props;
-  const classes = cx([cssHeading, className, css]);
+  const { className, css, as = "h1" } = props;
+  const cssAs = cvAsType(as);
+  const classes = cx([cssAs, className, css]);
   return classes;
 };
